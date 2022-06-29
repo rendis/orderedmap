3ef78d5d7ce0efb7830a1d71615d9eaab5cc0924
Reynaldo Endis <reynaldo.endis@gmail.com>
2022-06-28 23:46:09 -0400

    encapsulated structs

--- iterator.go
+++ iterator.go
@@ -2,39 +2,47 @@ package orderedmap
 
 // Iterator is an iterator over a OrderedMap.
 type Iterator[T any] interface {
+	// HasNext returns true if the omIter has a next element.
 	HasNext() bool
+
+	// Next moves the omIter to the next element and returns true if there was a next element in the omIter.
 	Next() bool
+
+	// GetNext returns the next element in the omIter.
 	GetNext() (T, bool)
+
+	// GetCurrent returns the current element and true if there was a current element in the omIter.
 	GetCurrent() (T, bool)
+
+	// GetCurrentV returns the current element, the index and true if there was a current element in the omIter.
 	GetCurrentV() (T, int, bool)
 }
 
+// NewIterator returns a new Iterator.
 func NewIterator[T any](values []T) Iterator[T] {
-	return &OMIterator[T]{
+	return &omIter[T]{
 		values: values,
 		index:  -1,
 		vlen:   len(values) - 1,
 	}
 }
 
-// OMIterator is an Iterator implementation.
-type OMIterator[T any] struct {
+// omIter is an Iterator implementation.
+type omIter[T any] struct {
 	index   int
 	values  []T
 	vlen    int
 	current T
 }
 
-// HasNext returns true if the OMIterator has a next element.
-func (i *OMIterator[T]) HasNext() bool {
+func (i *omIter[T]) HasNext() bool {
 	if i.index < i.vlen {
 		return true
 	}
 	return false
 }
 
-// Next moves the OMIterator to the next element and returns true if there was a next element in the OMIterator.
-func (i *OMIterator[T]) Next() bool {
+func (i *omIter[T]) Next() bool {
 	if i.HasNext() {
 		i.index++
 		i.current = i.values[i.index]
@@ -43,8 +51,7 @@ func (i *OMIterator[T]) Next() bool {
 	return false
 }
 
-// GetNext returns the next element in the OMIterator.
-func (i *OMIterator[T]) GetNext() (T, bool) {
+func (i *omIter[T]) GetNext() (T, bool) {
 	if i.HasNext() {
 		i.index++
 		t := i.values[i.index]
@@ -60,16 +67,14 @@ func (i *OMIterator[T]) GetNext() (T, bool) {
 	return *new(T), false
 }
 
-// GetCurrent returns the current element and true if there was a current element in the OMIterator.
-func (i *OMIterator[T]) GetCurrent() (T, bool) {
+func (i *omIter[T]) GetCurrent() (T, bool) {
 	if i.index > -1 {
 		return i.current, true
 	}
 	return *new(T), false
 }
 
-// GetCurrentV returns the current element, the index and true if there was a current element in the OMIterator.
-func (i *OMIterator[T]) GetCurrentV() (T, int, bool) {
+func (i *omIter[T]) GetCurrentV() (T, int, bool) {
 	if i.index > -1 {
 		return i.current, i.index, true
 	}
--- orderedmap.go
+++ orderedmap.go
@@ -1,59 +1,82 @@
 package orderedmap
 
-// New creates a new OrderedMap
+// New return a new OrderedMap
 func New[T any]() OrderedMap[T] {
-	return &OMap[T]{
+	return &om[T]{
 		data: make(map[any]T),
 	}
 }
 
 // OrderedMap is a map with ordered keys.
 type OrderedMap[T any] interface {
+	// Len returns the number of elements in the map.
 	Len() int
+
+	// Set sets the value for the given key.
 	Set(key any, val T)
+
+	// Get returns the value for the given key.
+	// If the key does not exist, the second argument will be false.
 	Get(key any) (T, bool)
+
+	// Delete deletes the value for the given key.
+	// Returns deleted value, true if the key is found.
 	Delete(key any) (T, bool)
+
+	// Keys returns the keys of the map.
 	Keys() []any
+
+	// Values returns the values of the map.
 	Values() []T
+
+	// Exists returns true if the key exists in the map.
 	Exists(key any) bool
+
+	// IndexOf returns the index of the given key.
+	// If the key does not exist, returns -1.
 	IndexOf(key any) int
+
+	// ReplaceKey replaces the key of the given key with the new key.
+	// Returns false if the key does not exist.
 	ReplaceKey(oldKey any, newKey any) bool
+
+	// SetBefore sets the new (newKey, value) pair before the given presentKey.
+	// Return false if presentKey does not exist or if newKey already exists.
 	SetBefore(presentKey any, newKey any, val T) (int, bool)
+
+	// SetAfter sets the new (newKey, value) pair after the given presentKey.
+	// Return false if presentKey does not exist or if newKey already exists.
 	SetAfter(presentKey any, newKey any, val T) (int, bool)
+
+	// Iterator returns an omIter for the map.
 	Iterator() Iterator[T]
 }
 
-// OMap is the OrderedMap implementation.
-type OMap[T any] struct {
+// om is the OrderedMap implementation.
+type om[T any] struct {
 	key  []any
 	data map[any]T
 }
 
-// Len returns the number of elements in the map.
-func (m *OMap[T]) Len() int {
+func (m *om[T]) Len() int {
 	return len(m.key)
 }
 
-// Set sets the value for the given key.
-func (m *OMap[T]) Set(key any, val T) {
+func (m *om[T]) Set(key any, val T) {
 	if _, ok := m.data[key]; !ok {
 		m.key = append(m.key, key)
 	}
 	m.data[key] = val
 }
 
-// Get returns the value for the given key.
-// If the key does not exist, the second argument will be false.
-func (m *OMap[T]) Get(key any) (T, bool) {
+func (m *om[T]) Get(key any) (T, bool) {
 	if v, ok := m.data[key]; ok {
 		return v, true
 	}
 	return *new(T), false
 }
 
-// Delete deletes the value for the given key.
-// Returns deleted value, true if the key is found.
-func (m *OMap[T]) Delete(key any) (T, bool) {
+func (m *om[T]) Delete(key any) (T, bool) {
 	if v, ok := m.data[key]; ok {
 		delete(m.data, key)
 		return v, true
@@ -61,13 +84,11 @@ func (m *OMap[T]) Delete(key any) (T, bool) {
 	return *new(T), false
 }
 
-// Keys returns the keys of the map.
-func (m *OMap[T]) Keys() []any {
+func (m *om[T]) Keys() []any {
 	return m.key
 }
 
-// Values returns the values of the map.
-func (m *OMap[T]) Values() []T {
+func (m *om[T]) Values() []T {
 	vals := make([]T, len(m.key))
 	for i, k := range m.key {
 		vals[i] = m.data[k]
@@ -75,15 +96,12 @@ func (m *OMap[T]) Values() []T {
 	return vals
 }
 
-// Exists returns true if the key exists in the map.
-func (m *OMap[T]) Exists(key any) bool {
+func (m *om[T]) Exists(key any) bool {
 	_, ok := m.data[key]
 	return ok
 }
 
-// IndexOf returns the index of the given key.
-// If the key does not exist, returns -1.
-func (m *OMap[T]) IndexOf(key any) int {
+func (m *om[T]) IndexOf(key any) int {
 	for i, k := range m.key {
 		if k == key {
 			return i
@@ -92,9 +110,7 @@ func (m *OMap[T]) IndexOf(key any) int {
 	return -1
 }
 
-// ReplaceKey replaces the key of the given key with the new key.
-// Returns false if the key does not exist.
-func (m *OMap[T]) ReplaceKey(oldKey any, newKey any) bool {
+func (m *om[T]) ReplaceKey(oldKey any, newKey any) bool {
 	if _, ok := m.data[oldKey]; !ok {
 		return false
 	}
@@ -108,20 +124,15 @@ func (m *OMap[T]) ReplaceKey(oldKey any, newKey any) bool {
 	return true
 }
 
-// SetBefore sets the new (newKey, value) pair before the given presentKey.
-// Return false if presentKey does not exist or if newKey already exists.
-func (m *OMap[T]) SetBefore(presentKey any, newKey any, value T) (int, bool) {
+func (m *om[T]) SetBefore(presentKey any, newKey any, value T) (int, bool) {
 	return m.setAt(presentKey, newKey, value, 0)
 }
 
-// SetAfter sets the new (newKey, value) pair after the given presentKey.
-// Return false if presentKey does not exist or if newKey already exists.
-func (m *OMap[T]) SetAfter(presentKey any, newKey any, value T) (int, bool) {
+func (m *om[T]) SetAfter(presentKey any, newKey any, value T) (int, bool) {
 	return m.setAt(presentKey, newKey, value, 1)
 }
 
-// Iterator returns an OMIterator for the map.
-func (m *OMap[T]) Iterator() Iterator[T] {
+func (m *om[T]) Iterator() Iterator[T] {
 	values := make([]T, 0, len(m.key))
 	for _, key := range m.key {
 		values = append(values, m.data[key])
@@ -129,7 +140,7 @@ func (m *OMap[T]) Iterator() Iterator[T] {
 	return NewIterator[T](values)
 }
 
-func (m *OMap[T]) setAt(presentKey any, newKey any, val T, delta int) (int, bool) {
+func (m *om[T]) setAt(presentKey any, newKey any, val T, delta int) (int, bool) {
 	if _, ok := m.data[presentKey]; !ok {
 		return 0, false
 	}
